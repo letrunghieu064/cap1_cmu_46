170cb3627ac334b71a59fa32cbdf3e425e06f1c9
GiaBao67 <lenguyengiabao6701@gmail.com>
2022-10-25 11:09:33 +0700

    modify edit profile

--- backend/config/routes.rb
+++ backend/config/routes.rb
@@ -9,7 +9,7 @@ Rails.application.routes.draw do
   # resources :wards
   # resources :districts
   # resources :posts
-  # resources :users
+  resources :users
   # resources :comments
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
