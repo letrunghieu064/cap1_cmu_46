be20f68ba0715ac85f91900b2fdc0b6ae8b74f44
GiaBao67 <lenguyengiabao6701@gmail.com>
2022-11-11 00:13:59 +0700

    initial react with rails

--- backend/app/controllers/application_controller.rb
+++ backend/app/controllers/application_controller.rb
@@ -1,2 +1,11 @@
 class ApplicationController < ActionController::API
+  include ActionController::RequestForgeryProtection
+  protect_from_forgery with: :null_session
+
+  after_action  :set_access_control_headers
+
+  def set_access_control_headers
+    headers['Access-Control-Allow-Origin'] = '*'
+  end
+
 end
