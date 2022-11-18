e15689f412fc2fd7dfe69bf0a972c3fe4fb26882
GiaBao67 <lenguyengiabao6701@gmail.com>
2022-11-18 23:33:09 +0700

    change model

--- backend/app/controllers/api/v1/posts_controller.rb
+++ backend/app/controllers/api/v1/posts_controller.rb
@@ -35,15 +35,6 @@ class Api::V1::PostsController < ApplicationController
       render json: @post.errors, status: :unprocessable_entity
     end
 
-    # respond_to do |format|
-    #   if @post.save
-    #     format.html { redirect_to post_url(@post), notice: "Post was successfully created." }
-    #     format.json { render :show, status: :created, location: @post }
-    #   else
-    #     format.html { render :new, status: :unprocessable_entity }
-    #     format.json { render json: @post.errors, status: :unprocessable_entity }
-    #   end
-    # end
   end
 
   # PATCH/PUT /posts/1 or /posts/1.json
@@ -55,25 +46,12 @@ class Api::V1::PostsController < ApplicationController
       render json: @post.errors, status: :unprocessable_entity
     end
 
-    # respond_to do |format|
-    #   if @post.update(post_params)
-    #     format.html { redirect_to post_url(@post), notice: "Post was successfully updated." }
-    #     format.json { render :show, status: :ok, location: @post }
-    #   else
-    #     format.html { render :edit, status: :unprocessable_entity }
-    #     format.json { render json: @post.errors, status: :unprocessable_entity }
-    #   end
-    # end
   end
 
   # DELETE /posts/1 or /posts/1.json
   def destroy
     @post.destroy
 
-    # respond_to do |format|
-    #   format.html { redirect_to posts_url, notice: "Post was successfully destroyed." }
-    #   format.json { head :no_content }
-    # end
   end
 
   def correct_user
@@ -88,6 +66,6 @@ class Api::V1::PostsController < ApplicationController
 
     # Only allow a list of trusted parameters through.
     def post_params
-      params.require(:post).permit(:name, :img_url, :description, :status, :address, :user_id, :district_id)
+      params.require(:post).permit(:name, :img_url, :description, :status, :address, :longitude, :latitude, :user_id)
     end
 end
--- backend/app/models/post.rb
+++ backend/app/models/post.rb
@@ -1,9 +1,8 @@
 class Post < ApplicationRecord
   belongs_to :users, optional: true
-  belongs_to :districts, optional: true
   has_many :likes
   has_many :comments
 
   geocoded_by :address
-  after_validation :geocode, if: ->(obj){ obj.address.present? and obj.name_changed? }
+  after_validation :geocode, if: ->(obj){ obj.address.present? and obj.address_changed? }
 end
