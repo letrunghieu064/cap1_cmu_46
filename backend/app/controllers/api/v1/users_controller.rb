53158a57b55cc4d1e861d50f32a4ac0b7c54bd02
GiaBao67 <lenguyengiabao6701@gmail.com>
2022-11-18 20:09:07 +0700

    create post with correct user

--- backend/app/controllers/api/v1/users_controller.rb
+++ backend/app/controllers/api/v1/users_controller.rb
@@ -28,8 +28,8 @@ class Api::V1::UsersController < ApplicationController
 
   # PATCH/PUT /users/1 or /users/1.json
   def update
-    if @user.update(user_params)
-      render json: @user
+    if current_user.update(user_params_update)
+      render json: current_user
     else
       render json: @user.errors, status: :unprocessable_entity
     end
@@ -50,6 +50,10 @@ class Api::V1::UsersController < ApplicationController
     # Only allow a list of trusted parameters through.
     def user_params
       params.require(:user).permit(:email, :first_name, :last_name, :username, :password, :birthday, :url_img, :phone_number, :address, :gender, :card_id, :role, :reset_password_token, :reset_password_at, :confirmation_token, :confirmation_at, :lock_at, :count_lock, :encrypted_password)
+    end
+
+    def user_params_update
+      params.require(:user).permit(:first_name, :last_name, :birthday, :url_img, :phone_number, :address, :gender, :card_id, :role)
     end
 
 end
