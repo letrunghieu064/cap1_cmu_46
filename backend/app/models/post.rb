5aecc0749a4c4dec42fef6bc6b46aed05c7dda4b
GiaBao67 <lenguyengiabao6701@gmail.com>
2022-11-18 21:17:49 +0700

    modify database

--- backend/app/models/post.rb
+++ backend/app/models/post.rb
@@ -3,4 +3,7 @@ class Post < ApplicationRecord
   belongs_to :districts, optional: true
   has_many :likes
   has_many :comments
+
+  geocoded_by :address
+  after_validation :geocode, if: ->(obj){ obj.address.present? and obj.name_changed? }
 end
